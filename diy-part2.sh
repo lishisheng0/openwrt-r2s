22f4d0019d45e84e03177657118180d8338e3ff3
lishisheng0 <1094346379@qq.com>
2025-01-10 10:20:11 +0800

    从 openwrt-r3s 复制过来

---
+++ diy-part2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i "s/192.168.1/192.168.0/" package/base-files/files/bin/config_generate
+sed -i 's/UTC/CST-8/' package/base-files/files/bin/config_generate
+
+sed -i 's/option lang auto/option lang zh_cn/' feeds/luci/modules/luci-base/root/etc/config/luci
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+
+#sed -i "/mediaurlbase/d" package/feeds/*/luci-theme*/root/etc/uci-defaults/*
+
+sed -i "s/^.*vermagic$/\techo '1' > \$(LINUX_DIR)\/.vermagic/" include/kernel-defaults.mk
+
+if [ -d "feeds/luci/applications/luci-app-dockerman" ]; then
+    rm -rf feeds/luci/applications/luci-app-dockerman
+fi
+# 判断并移动 feeds/kiddin9/luci-app-dockerman
+if [ -d "feeds/kiddin9/luci-app-dockerman" ]; then
+    cp -rf feeds/kiddin9/luci-app-dockerman feeds/luci/applications/
+fi
